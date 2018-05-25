fb8ecde69cba32b603eaa70e90a1c6b7fb24c4b0
Oscar René Toledo <oscartoledo.1994@gmail.com>
2018-05-24 21:48:08 -0600

    Agregarmos los archivos del modulo de seguridad, BD(01Tablas.sql, 02Restricciones.sql, 03Procedimientos.sql), Clases(Usuario.cs), Formularios(UsuarioAgregar, UsuarioModificar, UsuarioEliminar, UsuarioConsulta), tambien se cambio el nombre de Form1 por UsuarioLogin  Nota: Solo agregamos los archivos sin programar nada

--- Restaurante/Restaurante/UsuarioModificar.cs
+++ Restaurante/Restaurante/UsuarioModificar.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Restaurante
 {
-    public partial class Form1 : Form
+    public partial class UsuarioModificar : Form
     {
-        public Form1()
+        public UsuarioModificar()
         {
             InitializeComponent();
         }
