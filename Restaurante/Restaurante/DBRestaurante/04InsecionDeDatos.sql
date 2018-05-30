70653a5423dd2957abe8ff6228bdcf7a00787522
Oscar René Toledo <oscartoledo.1994@gmail.com>
2018-05-30 17:52:21 -0600

    correccion de errores en la insercion de datos del archivo sql

--- Restaurante/Restaurante/DBRestaurante/04InsecionDeDatos.sql
+++ Restaurante/Restaurante/DBRestaurante/04InsecionDeDatos.sql
@@ -2,16 +2,21 @@ INSERT INTO Restaurante.Usuarios
 (
     usuario,	--el usuario se define como 
 	clave,
+	nombre,
+	apellido,
     idArea
 )
 VALUES
-(   N'Oscar', -- usuario - nvarchar(20)
-    N'Ninguna', -- clave - nvarchar(20)
+(   N'OToledo', -- usuario - nvarchar(20)
+	N'Ninguna', -- clave - nvarchar(20)
+    N'Oscar', --nombre - nvarchar (25)
+	N'Toledo', --apellido - nvarchar (25)
+
     1    -- idArea - int
 )
 GO
 
-INSERT INTO Restaurante.Area
+INSERT INTO Restaurante.Areas
 (
     nombre
 )
@@ -23,4 +28,4 @@ VALUES
 
 --AREA DE PRUEBA DE LA INFORMACION
 SELECT * FROM Restaurante.Usuarios
-SELECT * FROM Restaurante.Area
+SELECT * FROM Restaurante.Areas
