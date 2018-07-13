2dc6c9f22ba951abc10d3aea0b29d898b76d1461
Oscar René Toledo <oscartoledo.1994@gmail.com>
2018-07-12 19:25:28 -0600

    30% Realizacion de procedimientos

--- Restaurante/Restaurante/DBRestaurante/04Procedimientos.sql
+++ Restaurante/Restaurante/DBRestaurante/04Procedimientos.sql
@@ -126,35 +126,117 @@ BEGIN
 		
 END
 GO
-CREATE PROCEDURE SP_AgregarTipoAcceso
+
+CREATE PROCEDURE SP_InsertarTipoAcceso
 (
+	@departamento NVARCHAR(20)
+	--pendientes los parametros de los modulos 
+	--que contendra el sistema
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(Acceso.TipoAcceso.departamento) FROM Acceso.TipoAcceso WHERE departamento = @departamento;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Ya existe un departamento con el nombre  "%s"', 16, 1, @departamento);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(departamento)
+				VALUES(	@departamento)
+			RETURN 1
+		END
 END
 GO
+EXEC SP_InsertarTipoAcceso 'Cocina'
+select * from Acceso.TipoAcceso
+
 
 CREATE PROCEDURE SP_ModificarTipoAcceso
 (
+	@id INT,
+	@departamento VARCHAR(20)
+	--pendientes los parametros de los modulos 
+	--que contendra el sistema
+	
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(Acceso.TipoAcceso.id) FROM Acceso.TipoAcceso WHERE id=@id;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'No existe el departamento con el id %d', 16, 1, @id);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			--ANTES DE REALIZAR LA INSERCION VERIFICAR QUE EL NOMBRE NUEVO DEL
+			--DEPARTAMENTO A MODIFICAR NO ESTE EN EL SISTEMA
+			UPDATE Acceso.TipoAcceso
+				SET 	departamento=@departamento
+					WHERE id=@id;
+			RETURN 1
+		END
 END
 GO
+exec SP_ModificarTipoAcceso 21,'Meseros'
+select * from Acceso.TipoAcceso
 
 CREATE PROCEDURE SP_EliminarTipoAcceso
 (
+	@id INT
 )
 AS 
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(Acceso.TipoAcceso.id) FROM Acceso.TipoAcceso WHERE id=@id;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'No existe el departamento con el id %d"', 16, 1, @id);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM Acceso.TipoAcceso WHERE id=@id;
+				RETURN 1
+			END
 END
 GO
+exec SP_EliminarTipoAcceso 11
+select * from Acceso.TipoAcceso
+
 
 CREATE PROCEDURE SP_AgregarProveedor
 (
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -163,6 +245,25 @@ CREATE PROCEDURE SP_ModificarProveedor
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -172,7 +273,19 @@ CREATE PROCEDURE SP_EliminarProveedor
 )
 AS
 BEGIN
-	DELETE FROM Restaurante.Proveedores WHERE idProveedor = @idProveedor;
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
 
@@ -181,6 +294,22 @@ CREATE PROCEDURE SP_AgregarMesero
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -188,7 +317,26 @@ CREATE PROCEDURE SP_ModificarMesero
 (
 )
 AS
-BEGIN 
+BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -197,30 +345,94 @@ CREATE PROCEDURE SP_EliminarMesero
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
 
-CREATE PROCEDURE SP_AgregarArea
+CREATE PROCEDURE SP_InsertarArea
 (
+	@nombre VARCHAR(50)
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(Restaurante.Areas.nombre) FROM Restaurante.Areas WHERE nombre=@nombre;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Ya existe una area con el nombre "%s"', 16, 1,@nombre);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Restaurante.Areas(nombre)
+				VALUES(@nombre)
+			RETURN 1
+		END
 END
 GO
+
+exec SP_InsertarArea 'Terraza'
+select * from Restaurante.Areas
 
 CREATE PROCEDURE SP_ModificarArea
 (
+	@id INT,
+	@nombre VARCHAR(50)
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+	SELECT @existe = COUNT(Restaurante.Areas.id) FROM Restaurante.Areas WHERE id=@id;
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'No existe ninguna Area con el id "%d"', 16, 1, @id);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE Restaurante.Areas
+				SET 	nombre = @nombre
+					WHERE id = @id;
+			RETURN 1
+		END
 END
 GO
 
 CREATE PROCEDURE SP_EliminarArea
 (
+	@id INT
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+	SELECT @existe = COUNT(Restaurante.Areas.id) FROM Restaurante.Areas WHERE id=@id;
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'No existe ninguna Area con el id "%d"', 16, 1, @id);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			DELETE FROM Restaurante.Areas	WHERE id = @id;
+			RETURN 1
+		END
 END
 GO
 
@@ -229,6 +441,22 @@ CREATE PROCEDURE SP_AgregarMesa
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -237,6 +465,25 @@ CREATE PROCEDURE SP_ModificarMesa
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -245,6 +492,19 @@ CREATE PROCEDURE SP_EliminarMesa
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
 
@@ -253,6 +513,22 @@ CREATE PROCEDURE SP_AgregarPedido
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -261,6 +537,25 @@ CREATE PROCEDURE SP_ModificarPedido
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -269,6 +564,19 @@ CREATE PROCEDURE SP_EliminarPedido
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
 
@@ -277,6 +585,22 @@ CREATE PROCEDURE SP_AgregarFactura
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -285,6 +609,7 @@ CREATE PROCEDURE SP_AgregarInventario
 )
 AS
 BEGIN
+
 END
 GO
 
@@ -292,7 +617,26 @@ CREATE PROCEDURE SP_ModificarInventario
 (
 )
 AS 
-BEGIN 
+BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -301,6 +645,19 @@ CREATE PROCEDURE SP_EliminarInventario
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
 
@@ -309,6 +666,22 @@ CREATE PROCEDURE SP_AgregarInsumo
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+	IF (@existe > 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+			
+		END
+	ELSE
+		BEGIN
+			INSERT INTO Acceso.TipoAcceso(<campo1>, <campo2>,.... ,<campoN>)
+				VALUES(	<variable1>, <variable2>,...,<Variable3>)
+			RETURN 1
+		END
 END
 GO
 
@@ -317,6 +690,25 @@ CREATE PROCEDURE SP_ModificarInsumo
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+
+	SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+
+	IF (@existe = 0)
+		BEGIN
+			RAISERROR(N'Aqui va el mensaje de error"', 16, 1);
+			RETURN 0
+		END 	
+	ELSE
+		BEGIN
+			UPDATE <esquema.tabla>
+				SET 	<campos=variables>
+					WHERE <condicion>;
+			RETURN 1
+		END
+	
+	END
 END
 GO
 
@@ -325,5 +717,18 @@ CREATE PROCEDURE SP_EliminarInsumo
 )
 AS
 BEGIN
+	DECLARE @existe int;
+	SET @existe = 0;
+		SELECT @existe = COUNT(<esquema.tabla.campo>) FROM <Esquema.tabla> WHERE <condicion>;
+		IF (@existe = 0)
+			BEGIN
+				RAISERROR(N'aqui va el mensaje de error "', 16, 1);
+				RETURN 0
+			END 	
+		ELSE
+			BEGIN
+				DELETE FROM <Esquema.tabla>	WHERE <condicion>;
+				RETURN 1
+			END
 END
 GO
