b5b9eed393b6b4916ac3f735ca98fc65da031fc3
Oscar René Toledo <oscartoledo.1994@gmail.com>
2018-08-13 03:37:17 -0600

    Creacion de las prefacturas, y enlace con las imagenes en red

--- Restaurante/Restaurante/Program.cs
+++ Restaurante/Restaurante/Program.cs
@@ -16,7 +16,7 @@ namespace Restaurante
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ModuloFacturas());
+            Application.Run(new ModuloUsuarios());
         }
     }
 }
