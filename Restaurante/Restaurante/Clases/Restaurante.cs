1fe54ea46d33fbc5701e727dfb6b0dc787e0ef08
Oscar René Toledo <oscartoledo.1994@gmail.com>
2018-08-13 09:59:52 -0600

    Definiciones en restaurante

--- Restaurante/Restaurante/Clases/Restaurante.cs
+++ Restaurante/Restaurante/Clases/Restaurante.cs
@@ -603,6 +603,74 @@ namespace Restaurante.Clases
             }
         }
 
+        private static void ValidarArea
+(
+   string nombre,
+   int numeroMesas
+)
+        {
+            if (nombre.Length == 0 || numeroMesas == 0)
+            {
+                throw new Clases.Exepcion
+                    (
+                    "Error al insertar una Area. \n\n" +
+                    "Existen datos obligatorios que se necesitan para poder agregar una area:\n" +
+                    "Nombre   : Area Familiar\n" +
+                    "Número Mesas : 10",
+                    new Exception(),
+                    "Clase_Restaurante"
+                    );
+            }
+        }
+        public static void AgregarAreas(string nombre, int numeroMesas)
+        {
+            try
+            {
+                ValidarArea(nombre, numeroMesas);
+                Clases.Areas area = new Clases.Areas(
+                    nombre,
+                    numeroMesas
+                    );
+                area.Agregar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void ModificarAreas(int id, string nombre, int numeroMesas)
+        {
+            try
+            {
+                ValidarArea(nombre, numeroMesas);
+                Clases.Areas area = new Clases.Areas(
+                    id,
+                    nombre,
+                    numeroMesas
+                    );
+                area.Modificar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public static void EliminarAreas(int id)
+        {
+            try
+            {
+
+                Clases.Areas area = new Clases.Areas(
+                    id
+                    );
+                area.Eliminar();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
 
 
     }
